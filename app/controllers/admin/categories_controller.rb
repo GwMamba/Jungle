dd5c26c720490f4ffed9df7cb71df85a78d6267b
GwMamba <gerald.mwang@gmail.com>
2024-01-12 23:19:35 +0900

    Instantiate a new Category object and redirect to categories page.

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,27 @@
+class Admin::CategoriesController < ApplicationController
+  before_action :require_admin
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def require_admin
+  end
+    
+end
