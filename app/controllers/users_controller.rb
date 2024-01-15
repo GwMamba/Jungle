3d6c8fdef1e0bbd4c7de5b1f5fa1ad8b96dc55a8
GwMamba <gerald.mwang@gmail.com>
2024-01-15 22:44:49 +0900

    logic to create action and privete user_params to sanitize the input form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(new_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+private
+  def new_params
+    params.require(:user).permit(:name, :email, :password_confirmation)
   end
 end
+```
