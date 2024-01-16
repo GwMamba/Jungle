69744798050d0b7c1cee9b3272739d232c545d33
GwMamba <gerald.mwang@gmail.com>
2024-01-16 20:27:35 +0900

    reflective of current sale status.

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,15 @@
 class Sale < ApplicationRecord
+
+  def Ended?
+    ends_on < Date.current
+  end
+
+  def Upcoming?
+    sale.starts_on > Date.current
+  end
+
+  def Active?
+    !Upcoming? && !Ended?
+  end
+
 end
