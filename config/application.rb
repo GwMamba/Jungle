08d658aba40b41b6e49676f3311016241e9bc8ae
GwMamba <gerald.mwang@gmail.com>
2025-04-11 21:49:47 +0900

    Modify application configuration settings

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,20 @@ module New
     # Load Google Cloud API key from config file
     config.before_configuration do
       env_file = Rails.root.join("config", "google_cloud.yml")
-      YAML.load(File.open(env_file))[Rails.env]/each do |key, value|
+      YAML.load(File.open(env_file))[Rails.env].each do |key, value|
         ENV[key.to_s] = value
       end if File.exist?(env_file)
     end  
     
+    # Enable i18n
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :en
+
+    # Set the locale from the user's language preference stored in session or cookie
+    # config.before_configuration do
+    #   I18n.locale = ENV['LANG'] || config.i18n.default_locale
+    # end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
