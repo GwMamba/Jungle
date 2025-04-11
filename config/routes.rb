24a88ba0ea67e62959c8846654fb4fd996c20609
GwMamba <gerald.mwang@gmail.com>
2025-04-11 21:50:17 +0900

    Update routes to include new controller actions or features

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,10 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  # Translation Routes
+  post '/translate' => 'translations#translate'
+  post '/change_language', to: 'application#change_language'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
